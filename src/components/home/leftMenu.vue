a7e54f2a91810fb4984f888dc28420e306a41aed
hy <976849662@qq.com>
2023-12-08 18:02:13 +0800

    美化滚动条

--- src/components/home/leftMenu.vue
+++ src/components/home/leftMenu.vue
@@ -4,7 +4,7 @@ import routes from '../../routes'
 
 <template>
   <div class="leftMenu">
-    <el-menu background-color="#545c64" active-text-color="#ffd04b" text-color="#fff">
+    <el-menu background-color="#434a50" active-text-color="#ffd04b" text-color="#fff">
       <template v-for="route in routes">
         <template v-if="!route.meta?.isPage">
           <el-menu-item :index="route.path" v-if="!route.children">
@@ -13,13 +13,13 @@ import routes from '../../routes'
               </el-icon>
               <span>{{ route.name }}</span>
             </template></el-menu-item>
-          <el-sub-menu :index="route.path" v-else>
+          <el-sub-menu  :index="route.path" v-else>
             <template #title>
               <el-icon>
               </el-icon>
               <span>{{ route.name }}</span>
             </template>
-            <el-menu-item v-for="cRoute in  route.children" :index="cRoute.path">{{ cRoute.name }}</el-menu-item>
+            <el-menu-item style="background: #545c64;" v-for="cRoute in  route.children" :index="cRoute.path">{{ cRoute.name }}</el-menu-item>
           </el-sub-menu>
         </template>
       </template>
@@ -30,8 +30,23 @@ import routes from '../../routes'
 <style>
 .leftMenu {
   background: #ccc;
+
+}
+.el-aside::-webkit-scrollbar{
+  width: 8px;
+}
+.el-aside::-webkit-scrollbar-track{ 
+  background: #f1f1f1;
 }
 
+.el-aside::-webkit-scrollbar-thumb {
+  background: #888;
+  border-radius: 4px;
+}
+
+.el-aside::-webkit-scrollbar-thumb:hover {
+  background: #555;
+}
 .el-menu-item {
   text-align: center;
 }
