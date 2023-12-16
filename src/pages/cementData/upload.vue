ad004d4afe4209c92df2ba8ecd02dd045cff801c
hy <976849662@qq.com>
2023-12-16 11:13:23 +0800

    listcontrain封装

--- src/pages/cementData/upload.vue
+++ src/pages/cementData/upload.vue
@@ -1,71 +1,61 @@
 <script setup>
 import { ref } from 'vue'
-import List from '../../components/list.vue'
+import ListContainer from '../../components/listContainer.vue'
 const tableData = ref([
   {
-    date: '2016-05-03',
-    name: 'Tom',
-    state: 'California',
-    city: 'Los Angeles',
-    address: 'No. 189, Grove St, Los Angeles',
-    zip: 'CA 90036',
-    tag: 'Home',
+    updated: '2016-05-03',
+    updatedCount: '+30',
+    dataTotal: '355',
+    desp: '*********',
   },
   {
-    date: '2016-05-02',
-    name: 'Tom',
-    state: 'California',
-    city: 'Los Angeles',
-    address: 'No. 189, Grove St, Los Angeles',
-    zip: 'CA 90036',
-    tag: 'Office',
+    updated: '2016-05-03',
+    updatedCount: '+30',
+    dataTotal: '355',
+    desp: '*********',
+  }, {
+    updated: '2016-05-03',
+    updatedCount: '+30',
+    dataTotal: '355',
+    desp: '*********',
   },
-  {
-    date: '2016-05-04',
-    name: 'Tom',
-    state: 'California',
-    city: 'Los Angeles',
-    address: 'No. 189, Grove St, Los Angeles',
-    zip: 'CA 90036',
-    tag: 'Home',
-  },
-  {
-    date: '2016-05-01',
-    name: 'Tom',
-    state: 'California',
-    city: 'Los Angeles',
-    address: 'No. 189, Grove St, Los Angeles',
-    zip: 'CA 90036',
-    tag: 'Office',
-  },
+
 ])
 </script>
 
 <template>
-  <List>
-    <template #header>
-      <el-button type="primary">Primary</el-button>
+  <ListContainer :mock="{ data: tableData, total: tableData.length }">
+    <template #addBtn>
+      <el-button type="primary">更新数据集</el-button>
+      <el-button>导出</el-button>
     </template>
-    <template #search>
-      <el-input />
-      <el-button type="primary">Search</el-button>
-      <el-button>Reset</el-button>
+    <template #form>
+      <el-form style="display: grid;grid-template-columns: repeat(3, 1fr);gap: 20px;">
+        <el-form-item label="时间:">
+          <el-date-picker type="datetimerange" start-placeholder="请选择开始时间" end-placeholder="请选择结束时间"
+            format="YYYY-MM-DD HH:mm:ss" date-format="YYYY/MM/DD ddd" time-format="A hh:mm:ss" />
+        </el-form-item>
+        <el-form-item label="数据总量 (>):">
+          <el-input />
+        </el-form-item>
+        <el-form-item label="描述:">
+          <el-input />
+        </el-form-item>
+      </el-form>
     </template>
-    <template #table>
-      <el-table :data="tableData" style="width: 100%">
-        <el-table-column fixed prop="date" label="Date" width="150" />
-        <el-table-column prop="name" label="Name" width="120" />
-        <el-table-column prop="state" label="State" width="120" />
-        <el-table-column prop="city" label="City" width="120" />
-        <el-table-column prop="address" label="Address" width="600" />
-        <el-table-column prop="zip" label="Zip" width="120" />
-        <el-table-column fixed="right" label="Operations" width="120">
-          <template #default>
-            <el-button link type="primary" size="small">Detail</el-button>
-            <el-button link type="primary" size="small">Edit</el-button>
-          </template>
-        </el-table-column>
-      </el-table>
-    </template>
-  </List>
+    <el-table :data="tableData" style="width: 100%">
+      <el-table-column type="index" width="100" />
+      <el-table-column prop="updated" label="上传（更新）时间" />
+      <el-table-column prop="updatedCount" label="数据更新条数" />
+      <el-table-column prop="dataTotal" label="数据总量" />
+      <el-table-column prop="desp" label="描述" />
+      <el-table-column fixed="right" label="操作">
+        <template #default>
+          <el-button link type="primary" size="small">查看</el-button>
+          <el-button link type="primary" size="small">编辑</el-button>
+          <el-button link type="primary" size="small">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </ListContainer>
 </template>
--- src/pages/inventoryManagement/outgoingInformation.vue
+++ src/pages/inventoryManagement/outgoingInformation.vue
@@ -1,71 +1,231 @@
-<script setup>
-import { ref } from 'vue'
-import List from '../../components/list.vue'
-const tableData = ref([
-  {
-    date: '2016-05-03',
-    name: 'Tom',
-    state: 'California',
-    city: 'Los Angeles',
-    address: 'No. 189, Grove St, Los Angeles',
-    zip: 'CA 90036',
-    tag: 'Home',
+<template>
+  <div class="list-container">
+    <div v-if="showAddBtn" style="display: flex;justify-content: end;margin-bottom: 20px;">
+      <slot name="addBtn" />
+    </div>
+    <!-- 筛选 -->
+    <div v-if="showSearchForm">
+      <slot name="form" />
+      <!-- 查询 -->
+      <div style="margin-top: 20px;">
+        <el-button type="primary" :loading="searchDisabled" @click="handleSearch">
+          查询
+        </el-button>
+        <!-- 重置 -->
+        <el-button v-if="!hideReset" type="primary" @click="handleResetSearchForm">
+          重置
+        </el-button>
+      </div>
+      <!-- 上传文件 -->
+      <slot name="uploadFile" />
+      <el-divider />
+    </div>
+    <div v-loading="tableLoading" class="table-box">
+      <!-- 表格数据的一些操作 -->
+      <div class="table-action">
+        <div>
+          <slot name="action-left" />
+        </div>
+        <div>
+          <slot name="action-right" />
+        </div>
+      </div>
+      <slot />
+    </div>
+    <div v-if="paginationShow" class="page-box">
+      <el-pagination layout="total, sizes, prev, pager, next" :page-sizes="[20, 50, 80, 100]" :disabled="searchDisabled"
+        :total="tableTotal" :current-page.sync="pageNum" :page-size.sync="pageSize" @size-change="sizeChangeGetList"
+        @current-change="getList" />
+    </div>
+  </div>
+</template>
+
+<script>
+// 列表容器组件
+export default {
+  name: 'ListContainer',
+  props: {
+    // 接口函数
+    api: {
+      type: Function,
+      required: true
+    },
+    // 查询条件
+    params: {
+      type: Object,
+      default: null
+    },
+    // 搜索框对象的引用
+    formRef: {
+      type: Object,
+      default: null
+    },
+    // 隐藏充值按钮
+    hideReset: {
+      type: Boolean,
+      default: false
+    },
+    // 首次加载调用列表接口
+    callInterface: {
+      type: Boolean,
+      default: true
+    },
+    showAddBtn: {
+      type: Boolean,
+      default: true
+    },
+    // 不显示查询条件区域
+    showSearchForm: {
+      type: Boolean,
+      default: true
+    },
+    paginationShow: {
+      type: Boolean,
+      default: true
+    },
+    validate: {
+      type: Boolean,
+      default: false
+    },
+    selfValid: {
+      type: Boolean,
+      default: false
+    },
+    handleSelfValid: {
+      type: Function,
+      default: () => true
+    },
+    mock: {
+      type: Object,
+      default: () => { }
+    }
   },
-  {
-    date: '2016-05-02',
-    name: 'Tom',
-    state: 'California',
-    city: 'Los Angeles',
-    address: 'No. 189, Grove St, Los Angeles',
-    zip: 'CA 90036',
-    tag: 'Office',
+  data() {
+    return {
+      searchDisabled: false,
+      tableLoading: false,
+      list: [],
+      tableTotal: 0,
+      pageNum: 1,
+      pageSize: 20
+    }
   },
-  {
-    date: '2016-05-04',
-    name: 'Tom',
-    state: 'California',
-    city: 'Los Angeles',
-    address: 'No. 189, Grove St, Los Angeles',
-    zip: 'CA 90036',
-    tag: 'Home',
+  async mounted() {
+    await this.$nextTick(() => { }) // 因为子组件的钩子比父组件先执行，所以现在子父让所有页面渲染完再调取方法
+    if (this.callInterface) {
+      this.getList()
+    }
   },
-  {
-    date: '2016-05-01',
-    name: 'Tom',
-    state: 'California',
-    city: 'Los Angeles',
-    address: 'No. 189, Grove St, Los Angeles',
-    zip: 'CA 90036',
-    tag: 'Office',
-  },
-])
+  methods: {
+    // 查询
+    handleSearch() {
+      this.pageNum = 1
+      this.pageSize = 20
+      this.getValidList()
+    },
+    // 重置表单
+    handleResetSearchForm() {
+      this.formRef.resetFields()
+      this.$emit('reset')
+    },
+    sizeChangeGetList() {
+      this.pageNum = 1
+      this.getValidList()
+    },
+    // 根据是否校验走不同方法
+    getValidList() {
+      if (this.validate) {
+        this.formRef.validate((valid) => {
+          if (valid) {
+            this.getList()
+          }
+        })
+        return
+      }
+      // 自定义校验
+      if (this.selfValid && !this.handleSelfValid()) return
+
+      this.getList()
+    },
+    // 请求列表数据
+    getList() {
+      this.searchDisabled = true
+      this.tableLoading = true
+
+      if (this.mock.data) {
+        setTimeout(() => {
+          const { data, total } = this.mock
+          // mock 为假数据，方便测试
+          this.list = data || []
+          this.tableTotal = total || 0
+          this.$emit('search', data)
+          this.searchDisabled = false
+          this.tableLoading = false
+        }, 1000)
+        return
+      }
+
+      this.api(this.generateParams())
+        .then(({ data }) => {
+          this.list = data.data || []
+          this.tableTotal = data.total || 0
+          this.$emit('search', data)
+        })
+        .catch(() => {
+          this.tableTotal = 0
+          this.$emit('search', {})
+        })
+        .finally(() => {
+          debugger
+          this.searchDisabled = false
+          this.tableLoading = false
+        })
+    },
+    // 生成请求参数
+    generateParams() {
+      if (this.paginationShow) {
+        const { pageNum, pageSize } = this
+        return {
+          ...this.params,
+          pageNum,
+          pageSize
+        }
+      }
+      return { ...this.params }
+    }
+  }
+}
 </script>
 
-<template>
-  <List>
-    <template #header>
-      <el-button type="primary">Primary</el-button>
-    </template>
-    <template #search>
-      <el-input />
-      <el-button type="primary">Search</el-button>
-      <el-button>Reset</el-button>
-    </template>
-    <template #table>
-      <el-table :data="tableData" style="width: 100%">
-        <el-table-column fixed prop="date" label="Date" width="150" />
-        <el-table-column prop="name" label="Name" width="120" />
-        <el-table-column prop="state" label="State" width="120" />
-        <el-table-column prop="city" label="City" width="120" />
-        <el-table-column prop="address" label="Address" width="600" />
-        <el-table-column prop="zip" label="Zip" width="120" />
-        <el-table-column fixed="right" label="Operations" width="120">
-          <template #default>
-            <el-button link type="primary" size="small">Detail</el-button>
-            <el-button link type="primary" size="small">Edit</el-button>
-          </template>
-        </el-table-column>
-      </el-table>
-    </template>
-  </List>
-</template>
+<style scoped>
+.list-container {
+  display: flex;
+  padding: 10px 10px 20px 10px;
+  height: 100%;
+  flex-direction: column;
+}
+
+.table-action {
+  display: flex;
+  justify-content: space-between;
+  padding-bottom: 10px;
+}
+
+.page-box {
+  text-align: center;
+  height: 40px;
+  margin-top: 20px;
+}
+
+.el-pagination {
+  margin-bottom: 20px;
+}
+
+.transactionLimit .transactionLimit_table {
+  margin-top: 0;
+}
+
+.el-divider--horizontal {
+  margin: 15px 0;
+}
+</style>
